8961fb39dd4eef90d0d8d3c56f13d7562070350b
Atul Hirpara <omhirapara130@gmail.com>
2026-07-03 15:56:22 +0530

    changed email service

--- Backend/src/main/java/com/example/moneymanager/service/EmailService.java
+++ Backend/src/main/java/com/example/moneymanager/service/EmailService.java
@@ -2,35 +2,85 @@ package com.example.moneymanager.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+//    private final JavaMailSender mailSender;
+//    @Value("${spring.mail.properties.mail.smtp.from}")
+//    private String fromEmail;
+//    public void sendMail(String to , String subject , String body){
+//
+//        if (to == null || to.isBlank()) {
+//            System.out.println("Email not sent because recipient email is null or empty.");
+//            return;
+//        }
+//
+//        try{
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom(fromEmail);
+//            message.setTo(to);
+//            message.setSubject(subject);
+//            message.setText(body);
+//            mailSender.send(message);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-    @Value("${spring.mail.properties.mail.smtp.from}")
+    // new code --------------------------------------------------------------
+    @Value("${brevo.api.key}")
+    private String apiKey;
+
+    @Value("${brevo.sender.email}")
     private String fromEmail;
-    public void sendMail(String to , String subject , String body){
 
-        if (to == null || to.isBlank()) {
-            System.out.println("Email not sent because recipient email is null or empty.");
-            return;
-        }
+    public void sendMail(String to, String subject, String body) {
 
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(fromEmail);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(body);
-            mailSender.send(message);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        headers.set("api-key", apiKey);
+
+        Map<String,Object> request = new HashMap<>();
+
+        Map<String,String> sender = new HashMap<>();
+        sender.put("email", fromEmail);
+        sender.put("name","Money Manager");
+
+        Map<String,String> receiver = new HashMap<>();
+        receiver.put("email",to);
+
+        request.put("sender",sender);
+
+        request.put("to", List.of(receiver));
+
+        request.put("subject",subject);
+
+        request.put("htmlContent",body);
+
+        HttpEntity<Map<String,Object>> entity =
+                new HttpEntity<>(request,headers);
+
+        restTemplate.postForEntity(
+                "https://api.brevo.com/v3/smtp/email",
+                entity,
+                String.class
+        );
 
     }
+
 }
